1d6c99f6da340ca98d8bd993e5dc29f04d9976aa
Aidan Feldman <aidan.feldman@gmail.com>
2012-10-26 04:15:20 -0400

    basic client lib

--- jsonp.js
+++ jsonp.js
@@ -7,7 +7,49 @@ MIT license
 */
 /*global jQuery:true*/
 (function($){
+  // groups: protocol, slashes, host, path
+  var regex = /^(?:(?:(?:(https?:))?(\/\/))?((?:[\w\-]\.?)+(?::\d+)?)?(\/\S*)?)$/i;
+
   $.jsonp = function(opts){
-     $.ajax(opts);
+    // make a copy
+    opts = $.extend({}, opts);
+
+    var url = opts.url,
+      match = url.match(regex), // not a valid URL unless matched
+      protocol = match[1],
+      host = match[3],
+      loc = window.location;
+
+    if (!protocol){
+      if (match[2]){
+        // protocol-relative
+        protocol = loc.protocol;
+      } else {
+        protocol = 'http:';
+      }
+    }
+
+    if (!host){
+      host = loc.host;
+    }
+
+    if (protocol === loc.protocol && host === loc.host){
+      // same domain
+    } else {
+      // needs proxying
+
+      // construct absolute URL
+      url = protocol + '//' + host + match[4];
+      url = '//jsonp.nodejitsu.com/?url=' + encodeURIComponent(url);
+
+      if (!$.support.cors){
+        // proxy via JSONP
+        url += '&callback=?';
+      }
+
+      opts.url = url;
+    }
+
+    $.ajax(opts);
   };
 }(jQuery));
--- test/client_test.js
+++ test/client_test.js
@@ -1,13 +1,88 @@
 /*jshint browser:true*/
 /*global describe, $, it, sinon, assert, expect */
 describe('$.jsonp()', function(){
-  it('should do standard ajax for relative domains', function(){
-    sinon.stub($, 'ajax');
+  function getOrigin(){
+    var loc = window.location;
+    return loc.origin || (loc.protocol + '//' + loc.host);
+  }
 
-    $.jsonp({
-      url: '/foo'
+  before(function(){
+    sinon.stub($, 'ajax');  
+  });
+
+
+  function sharedTests(){
+    it('should do standard ajax for relative domains', function(){
+      $.jsonp({
+        url: '/foo'
+      });
+
+      expect($.ajax.calledWithMatch({url: '/foo'})).to.be.ok();
     });
 
-    expect($.ajax.calledWithMatch({url: '/foo'})).to.be.ok();
+    it('should do standard ajax for the same domain', function(){
+      var url = getOrigin() + '/foo';
+
+      $.jsonp({
+        url: url
+      });
+
+      expect($.ajax.calledWithMatch({url: url})).to.be.ok();
+    });
+  }
+
+
+  describe('with CORS support', function(){
+    var origCors;
+    before(function(){
+      origCors = $.support.cors;
+      $.support.cors = true;
+    });
+
+    after(function(){
+      $.support.cors = origCors;
+    });
+
+
+    sharedTests();
+
+    it('should use the CORS proxy', function(){
+      var url = 'http://foo.com/bar';
+
+      $.jsonp({
+        url: url
+      });
+
+      expect($.ajax.calledWithMatch({
+        url: '//jsonp.nodejitsu.com/?url=' + encodeURIComponent(url)
+      })).to.be.ok();
+    });
+  });
+
+  describe('without CORS support', function(){
+    var origCors;
+    before(function(){
+      origCors = $.support.cors;
+      $.support.cors = false;
+    });
+
+    after(function(){
+      $.support.cors = origCors;
+    });
+
+
+    sharedTests();
+
+    it('should use the JSONP proxy', function(){
+      var url = 'http://foo.com/bar';
+
+      $.jsonp({
+        url: url
+      });
+
+      expect($.ajax.calledWithMatch({
+        url: '//jsonp.nodejitsu.com/?url=' + encodeURIComponent(url) + '&callback=?'
+      })).to.be.ok();
+    });
   });
 });
