14f455e85c246b8f41200c9be675e387977d65fd
Aidan Feldman <aidan.feldman@gmail.com>
2012-10-27 01:47:54 -0400

    client: refactoring

--- jsonp.js
+++ jsonp.js
@@ -15,45 +15,45 @@ MIT license
   //   cors {Boolean} Set to true if the URL is known to support CORS for this domain.
   //   jsonp {Boolean} Set to true if the URL is known to support JSONP.
   $.jsonp = function(opts){
+    var loc = window.location,
+      url = opts.url || loc.href, // jQuery.ajax() defaults to this
+      match = url.match(regex), // not a valid URL unless matched
+      host = match[3];
+
     // make a copy
     opts = $.extend({}, opts);
 
-    var url = opts.url || window.location.href, // jQuery.ajax() defaults to this
-      match = url.match(regex), // not a valid URL unless matched
-      protocol = match[1],
-      host = match[3],
-      loc = window.location;
-
     if (host && host !== loc.host){
       // requesting to a different domain
-
-      if (!protocol){
-        if (match[2]){
-          // protocol-relative
-          protocol = loc.protocol;
-        } else {
-          protocol = 'http:';
-        }
-      }
 
       if (!(opts.cors && $.support.cors) && !opts.jsonp){
         // server doesn't have cross-domain support
 
         // construct absolute URL
+        var protocol = match[1];
+        if (!protocol){
+          if (match[2]){
+            // protocol-relative
+            protocol = loc.protocol;
+          } else {
+            protocol = 'http:';
+          }
+        }
         url = protocol + '//' + host + match[4];
+
         // proxy the request
         url = '//jsonp.nodejitsu.com/?url=' + encodeURIComponent(url);
+        opts.url = url;
       }
 
       if (!$.support.cors){
         // proxy via JSONP
         opts.dataType = 'jsonp';
       }
-
-      delete opts.cors;
-      delete opts.jsonp;
-      opts.url = url;
     }
+
+    delete opts.cors;
+    delete opts.jsonp;
 
     $.ajax(opts);
   };
