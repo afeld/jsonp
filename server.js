ca805aa46f9de018fc690b88cd0220499d17fd00
Aidan Feldman <aidan.feldman@gmail.com>
2012-10-28 01:20:28 -0400

    set up for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-/*jshint node:true */
+/*jshint node:true, strict:false */
 var app = require('./lib/app.js');
 
-var apiPort = process.argv[2] || 8000;
-app.listen(apiPort);
-console.log('Server running at http://127.0.0.1:' + apiPort);
+var apiPort = process.argv[2] || process.env.PORT || 8000;
+app.listen(apiPort, function(){
+  console.log('Server running at http://127.0.0.1:' + apiPort);
+});
