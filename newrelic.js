08a98e4e3c9845ccd98a36e78359e6a24bcd7028
Aidan Feldman <afeld@github.com>
2014-04-01 01:10:06 -0400

    add new relic agent

---
+++ newrelic.js
@@ -0,0 +1,14 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ *
+ * https://docs.newrelic.com/docs/nodejs/configuring-nodejs-with-environment-variables
+ */
+exports.config = {
+  app_name : ['JSONP'],
+  logging : {
+    filepath : 'stdout'
+  }
+};
