85e1f5416e7994f167be1dd9e047d8ca133b85f6
Aidan Feldman <aidan.feldman@gsa.gov>
2015-11-11 22:48:53 -0500

    use redis for rate limiting

--- server/limiters/ip.js
+++ server/limiters/ip.js
@@ -7,11 +7,11 @@ const baseLimiter = require('./base');
 // use a function so the tests can be stateless
 module.exports = function(capacity) {
   capacity = capacity || 100;
-  let options = {
+  let options = baseLimiter.getOptions();
+  Object.assign(options, {
     userKey: 'connection.remoteAddress',
     capacity: capacity,
-    expire: 1000 * 60,
-    limitResponse: baseLimiter.getResponse()
-  };
+    expire: 1000 * 60
+  });
   return epsilonDelta(options);
 };
