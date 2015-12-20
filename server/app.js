9885a7b88a00fa4d724b74090729e61549b17d83
Aidan Feldman <aidan.feldman@gsa.gov>
2015-12-20 15:46:58 -0500

    add a new module for new relic instrumentation

--- server/app.js
+++ server/app.js
@@ -1,11 +1,7 @@
 /*jshint node:true */
 'use strict';
 
-if (process.env.NEW_RELIC_LICENSE_KEY) {
-  require('newrelic');
-} else {
-  console.warn("New Relic agent not being started because NEW_RELIC_LICENSE_KEY is missing.");
-}
+require('./instrument');
 
 const express = require('express'),
   morgan = require('morgan'),
