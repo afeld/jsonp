dc9d69396847a2ab92f99ddc953a59844a475607
Aidan Feldman <aidan.feldman@gsa.gov>
2016-11-26 03:11:17 -0600

    add a slow test server

---
+++ test/slow.js
@@ -0,0 +1,19 @@
+/*jshint node:true */
+// a simple server that serves a JSON file with a delay
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const delay = process.argv[2] || 500;
+const filePath = path.join(__dirname, '..', 'package.json');
+
+app.get('/', (req, res) => {
+  setTimeout(() => {
+    res.sendFile(filePath);
+  }, delay);
+});
+
+app.listen(3000, function () {
+  console.log('Ready at http://localhost:3000');
+});
