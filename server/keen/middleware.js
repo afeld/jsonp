b48a59d2b04209a37c80136b337cc645e52eceeb
Aidan Feldman <aidan.feldman@gmail.com>
2017-04-15 23:50:02 +0400

    extract common response start/end event info

--- server/keen/middleware.js
+++ server/keen/middleware.js
@@ -1,29 +1,32 @@
 const keen = require('./client');
 
-const registerResponse = (req, res, event, start) => {
+const getCommonEvent = (req) => {
+  return {
+    method: req.method,
+    path: req.path,
+    ip: req.ips[0]
+  };
+};
+
+const registerResponse = (req, res, resEvent, start) => {
   let end = new Date().getTime();
   let resTime = end - start; // milliseconds
   console.log(req);
   console.log(`response time:: ${resTime}ms`);
 
-  keen.recordEvent('reqEnd', {
-    event: event,
-    method: req.method,
-    path: req.path,
-    statusCode: res.statusCode,
-    resTime: resTime,
-    ip: req.ips[0]
-  });
+  const eventData = getCommonEvent(req);
+  eventData.event = resEvent;
+  eventData.statusCode = res.statusCode;
+  eventData.resTime = resTime;
+
+  keen.recordEvent('reqEnd', eventData);
 };
 
 module.exports = (req, res, next) => {
-  let start = new Date().getTime();
+  const start = new Date().getTime();
 
-  keen.recordEvent('reqStart', {
-    method: req.method,
-    path: req.path,
-    ip: req.ips[0]
-  });
+  const event = getCommonEvent(req);
+  keen.recordEvent('reqStart', event);
 
   res.on('close', () => {
     registerResponse(req, res, 'close', start);
@@ -31,5 +34,6 @@ module.exports = (req, res, next) => {
   res.on('finish', () => {
     registerResponse(req, res, 'finish', start);
   });
+
   next();
 };
