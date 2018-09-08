7706f9dcc0df19678ae84b249cd32607009fc149
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-08 03:00:09 -0400

    disable minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   entry: "./server/worker.js",
+  mode: "none",
   node: {
     // https://github.com/webpack-contrib/css-loader/issues/447#issuecomment-285598881
     fs: "empty",
