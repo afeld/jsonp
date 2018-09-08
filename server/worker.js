d68e4da70bb8ae04872c7c056ea2c498b67767e1
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-08 15:46:24 -0400

    express app as worker (broken)

--- server/worker.js
+++ server/worker.js
@@ -1,17 +1,4 @@
-const proxy = require('./proxy-request');
-const proxyUtil = require('./proxy_util');
-const url = require('url');
+const expressService = require('express-service');
+const app = require('./app');
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
-
-function getApiUrl(req) {
-  const query = url.parse(req.url, true).query;
-  return proxyUtil.getApiUrlFromQuery(query);
-}
-
-async function handleRequest(req) {
-  let apiUrl = getApiUrl(req);
-  return proxy(apiUrl, req);
-}
+expressService(app);
