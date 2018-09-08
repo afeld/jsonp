3515cef9afb84829674fdba7d665b19b94c1a8cf
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-08 02:27:45 -0400

    bypass some unneeded node modules in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   entry: "./server/worker.js",
+  node: {
+    // https://github.com/webpack-contrib/css-loader/issues/447#issuecomment-285598881
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  },
   // https://developers.cloudflare.com/workers/writing-workers/using-npm-modules/
   target: "webworker"
 };
