f80d75872fe2f517bdea3f3795bf76c7686899aa
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-08 04:01:50 -0400

    refactor worker

--- server/worker.js
+++ server/worker.js
@@ -3,12 +3,15 @@ const proxyUtil = require('./proxy_util');
 const url = require('url');
 
 addEventListener('fetch', event => {
-  event.respondWith(fetchAndApply(event.request));
+  event.respondWith(handleRequest(event.request));
 });
 
-async function fetchAndApply(req) {
+function getApiUrl(req) {
   const query = url.parse(req.url, true).query;
-  let apiUrl = proxyUtil.getApiUrlFromQuery(query);
-  const response = await proxy(apiUrl, req);
-  return response;
+  return proxyUtil.getApiUrlFromQuery(query);
+}
+
+async function handleRequest(req) {
+  let apiUrl = getApiUrl(req);
+  return proxy(apiUrl, req);
 }
