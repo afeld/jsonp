a78e2763e2ac0d12d0cdac669d0ae5ac1e841d22
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-09 15:48:00 -0400

    use CORS middleware for worker

--- server/worker.js
+++ server/worker.js
@@ -1,6 +1,12 @@
+const cors = require('cors');
 const proxy = require('./proxy-request');
 const proxyUtil = require('./proxy_util');
 const url = require('url');
+
+const corsMiddleware = cors({
+  maxAge: 60 * 60 * 24, // one day
+  methods: ['GET']
+});
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
@@ -13,5 +19,17 @@ function getApiUrl(req) {
 
 async function handleRequest(req) {
   let apiUrl = getApiUrl(req);
-  return proxy(apiUrl, req);
+  const proxyRes = await proxy(apiUrl, req);
+
+  // fetch() response isn't mutable, so make a new one
+  const res = new Response(proxyRes.body, {
+    status: proxyRes.status,
+    headers: proxyRes.headers
+  });
+
+  // make browser Response act like http.ServerResponse
+  res.setHeader = res.headers.set.bind(res.headers);
+  corsMiddleware(req, res, () => {});
+
+  return res;
 }
