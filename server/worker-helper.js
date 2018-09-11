942d0b7e66348815943f0a0c8be9971126fb83e4
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-10 21:49:40 -0400

    split worker registration from logic

---
+++ server/worker-helper.js
@@ -0,0 +1,32 @@
+const cors = require('cors');
+const proxy = require('./proxy-request');
+const proxyUtil = require('./proxy_util');
+const url = require('url');
+
+const corsMiddleware = cors({
+  maxAge: 60 * 60 * 24, // one day
+  methods: ['GET']
+});
+const emptyFn = () => {};
+
+function getApiUrl(req) {
+  const query = url.parse(req.url, true).query;
+  return proxyUtil.getApiUrlFromQuery(query);
+}
+
+module.exports = async function(req) {
+  let apiUrl = getApiUrl(req);
+  const proxyRes = await proxy(apiUrl, req);
+
+  // fetch() response isn't mutable, so make a new one
+  const res = new Response(proxyRes.body, {
+    status: proxyRes.status,
+    headers: proxyRes.headers
+  });
+
+  // make browser Response act like http.ServerResponse
+  res.setHeader = res.headers.set.bind(res.headers);
+  corsMiddleware(req, res, emptyFn);
+
+  return res;
+};
