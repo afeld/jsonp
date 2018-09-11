1a2c48e615eec95448fb58db1d01ee2963a77957
Aidan Feldman <aidan.feldman@gmail.com>
2018-09-11 02:24:03 -0400

    add a test for the worker to include the headers

--- test/server/worker_test.js
+++ test/server/worker_test.js
@@ -10,14 +10,14 @@ describe('CloudFlare Worker', () => {
     nock.cleanAll();
   });
 
-  it('should give a status of 502 for a non-existent page', () => {
+  it('gives a status of 502 for a non-existent page', () => {
     const req = new Request('http://jsonp.test/?url=http://localhost:8001');
     return handleRequest(req).catch(err => {
       expect(err.code).to.be('ECONNREFUSED');
     });
   });
 
-  it('should pass the JSON and set the CORS headers', async () => {
+  it('passes the JSON and set the CORS headers', async () => {
     const destHost = 'http://localhost:8001';
     const json = { message: 'test' };
     nock(destHost)
@@ -31,5 +31,20 @@ describe('CloudFlare Worker', () => {
     expect(res.headers.get('access-control-allow-origin')).to.eql('*');
     const data = await res.json();
     expect(data).to.eql(json);
+  });
+
+  it('includes the status and headers', async () => {
+    const destHost = 'http://localhost:8001';
+    nock(destHost)
+      .get('/')
+      .reply(202, '', {
+        'x-foo': 'bar'
+      });
+
+    const req = new Request(`http://jsonp.test/?url=${destHost}`);
+    const res = await handleRequest(req);
+
+    expect(res.status).to.be(202);
+    expect(res.headers.get('x-foo')).to.eql('bar');
   });
 });
