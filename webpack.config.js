50c66c099ee5c2bc7da30266cd22a4e5263eeb28
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-04 02:01:02 -0500

    shrink webpack output to get under CloudFlare limit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const webpack = require('webpack');
 
+// https://community.cloudflare.com/t/cloudflare-workers-webpack-boilerplate-rfc/30404/13
+const workerSizeLimit = 1000000;
+
 module.exports = {
   entry: './server/worker.js',
-  mode: 'none',
+  mode: 'production',
   node: {
     // https://github.com/webpack-contrib/css-loader/issues/447#issuecomment-285598881
     fs: 'empty',
     net: 'empty',
     tls: 'empty'
+  },
+  performance: {
+    maxAssetSize: workerSizeLimit,
+    maxEntrypointSize: workerSizeLimit
   },
   plugins: [
     new webpack.ProvidePlugin({
