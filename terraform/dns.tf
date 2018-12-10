ac049d63205779d46ddacf5731b0c7334750a0ca
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-09 23:05:46 -0500

    remove AWS terraform resources

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -8,23 +8,11 @@ provider "cloudflare" {
 resource "cloudflare_record" "main" {
   domain  = "${var.cloudflare_domain}"
   name    = "${var.cloudflare_subdomain}"
-  value   = "${aws_eip.proxy.public_ip}"
-  type    = "A"
   proxied = true
-}
 
-# https://github.com/terraform-providers/terraform-provider-cloudflare/issues/6#issuecomment-313375481
-data "http" "cloudflare_ipv4" {
-  url = "https://www.cloudflare.com/ips-v4"
-}
-
-data "http" "cloudflare_ipv6" {
-  url = "https://www.cloudflare.com/ips-v6"
-}
-
-locals {
-  cloudflare_ipv4_cidrs = ["${split("\n",trimspace(data.http.cloudflare_ipv4.body))}"]
-  cloudflare_ipv6_cidrs = ["${split("\n",trimspace(data.http.cloudflare_ipv6.body))}"]
+  // this is an arbitrary value, since all requests will be captured by the worker anyway
+  value = "52.15.126.6"
+  type  = "A"
 }
 
 resource "cloudflare_worker_script" "jsonp" {
--- terraform/serverless.tf
+++
@@ -1,7 +0,0 @@
-data "aws_cloudformation_stack" "serverless" {
-  name = "${var.cloudformation_stack}"
-}
-
-locals {
-  endpoint_url = "${data.aws_cloudformation_stack.serverless.outputs["ServiceEndpoint"]}/"
-}
