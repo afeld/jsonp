145428bdc8fd9bb16602f3ffe031d56469d80bfd
Aidan Feldman <aidan.feldman@gmail.com>
2022-01-18 21:09:05 -0500

    slim the karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,53 +1,15 @@
-// Karma configuration
-// Generated on Tue Jan 18 2022 21:02:14 GMT-0500 (Eastern Standard Time)
-
 module.exports = function (config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
     frameworks: ['mocha'],
-
-    // list of files / patterns to load in the browser
     files: ['**/*.js', '**/*.ts'],
-
-    // list of files / patterns to exclude
     exclude: ['node_modules'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
-    preprocessors: {},
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    // preprocessors: {},
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
     browsers: ['Chrome'], //, 'ChromeHeadless'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser instances should be started simultaneously
-    concurrency: Infinity,
   });
 };
